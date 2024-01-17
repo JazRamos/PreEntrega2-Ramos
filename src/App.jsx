1f7f836f287f328fab6fa7f98d4bc21823bf5bd0
Jaz Ramos <jazminaramos4@hotmail.com>
2024-01-16 22:17:16 -0300

    entrega pre2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-import { ItemListContainer } from "./Components/ItemListContainer/ItemListContainer"
-import { NavBar } from "./Components/NavBar/NavBar"
+import { ItemListContainer } from "./Components/ItemListContainer/ItemListContainer";
+import { ItemDetailContainer } from "./Components/ItemDetailContainer/ItemDetailContainer";
+import { NavBar } from './Components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <>
-    <NavBar />
-    <ItemListContainer message={"Bienvenidos a la mejor librería de alguna galaxia!"} />
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer greeting={"Bienvenidos a la mejor librería de alguna galaxia: Librería Arcoíris!"} />} />
+        <Route path="/category/:category" element={<ItemListContainer greeting={"Librería Arcoíris"} />} />
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
 
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,25 +1,34 @@
-import { CartWidget } from "../CartWidget/CartWidget";
+import { CartWidget } from '../CartWidget/CartWidget';
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid justify-content-center">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="nav" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link" href="#">Home</a>
-              <a className="nav-link" href="#">Artículos</a>
-              <a className="nav-link" href="#">Contacto</a>
-            </div>
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid justify-content-center">
+        <div className="nav" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link to={"/"}>
+              <button className="btn btn-outline-dark mx-2">Home</button>
+            </Link>
+            <Link to={"/category/lapiceras"}>
+              <button className="btn btn-outline-dark mx-2">Lapiceras</button>
+            </Link>
+            <Link to={"/category/cuadernos"}>
+              <button className="btn btn-outline-dark mx-2">Cuadernos</button>
+            </Link>
+            <Link to={"/category/resaltadores"}>
+              <button className="btn btn-outline-dark mx-2">Resaltadores</button>
+            </Link>
+            <Link to={"/category/lapices"}>
+              <button className="btn btn-outline-dark mx-2">Lapices</button>
+            </Link>
           </div>
         </div>
-        <div className=" d-flex ">
-            <CartWidget />
-            <p className="  badge border border-light rounded-circle bg-danger p-1">2</p>
-        </div>
-      </nav>
-    );
-  };
-  
+      </div>
+      <div className=" d-flex ">
+        <CartWidget />
+        <p className="badge border border-light rounded-circle bg-danger p-1">2</p>
+      </div>
+    </nav>
+  );
+};
